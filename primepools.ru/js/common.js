dc9d3f695787150fc8eb1ed8e06ee174c89a3a19
Max Dudin <mr.madu@ya.ru>
2017-09-01 14:48:58 +0300

    slider

--- primepools.ru/js/common.js
+++ primepools.ru/js/common.js
@@ -685,14 +685,14 @@ $(function() {
   });
 
   //Chrome Smooth Scroll
-  try {
-    $.browserSelector();
-    if($("html").hasClass("chrome")) {
-      $.smoothScroll();
-    }
-  } catch(err) {
+  // try {
+  //   $.browserSelector();
+  //   if($("html").hasClass("chrome")) {
+  //     $.smoothScroll();
+  //   }
+  // } catch(err) {
 
-  };
+  // };
 
   $("img, a").on("dragstart", function(event) { event.preventDefault(); });
 

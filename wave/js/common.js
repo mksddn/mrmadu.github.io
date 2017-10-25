aaf199b14e97903482903f4379185fa4ec751e56
Max Dudin <mr.madu@ya.ru>
2017-10-25 16:17:12 +0300

    gallery

--- wave/js/common.js
+++ wave/js/common.js
@@ -112,6 +112,48 @@ $(function() {
     mainClass: 'my-mfp-zoom-in'
   });
 
+  $('.gallery .link').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Идет загрузка #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">Изображение #%curr%</a> не может быть загружено.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>База отдыха ВОЛНА</small>';
+      }
+    }
+  });
+
+  // $('.gallery .link').magnificPopup({
+  //   delegate: 'a',
+  //   type: 'image',
+  //   // closeOnContentClick: false,
+  //   // closeBtnInside: false,
+  //   mainClass: 'mfp-with-zoom mfp-img-mobile',
+  //   image: {
+  //     verticalFit: true,
+  //     titleSrc: function(item) {
+  //       return item.el.attr('title') + ' <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank"></a>';
+  //     }
+  //   },
+  //   gallery: {
+  //     enabled: true
+  //   },
+  //   zoom: {
+  //     enabled: true,
+  //     duration: 300, // don't foget to change the duration also in CSS
+  //     opener: function(element) {
+  //       return element.find('img');
+  //     }
+  //   }
+  // });
+
 });
 
 

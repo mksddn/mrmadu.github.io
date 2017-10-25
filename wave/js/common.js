0ef1682191509e0f14810c9221d6c48466768bef
Max Dudin <mr.madu@ya.ru>
2017-10-25 13:17:04 +0300

    layout

--- wave/js/common.js
+++ wave/js/common.js
@@ -29,7 +29,106 @@ $(function() {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    dots: true
+    dots: true,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        dots: false
+      }
+    }
+  ]
   });
+
+});
+
+
+
+
+var spinner = $('.ymap-container').children('.loader');
+var check_if_load = false;
+var myMapTemp, myPlacemarkTemp;
+function init () {
+  var myMapTemp = new ymaps.Map("map-yandex", {
+    center: [44.679423, 37.627068], // координаты центра на карте
+    zoom: 13,
+    controls: ['zoomControl', 'fullscreenControl'] // выбираем только те функции, которые необходимы при использовании
+  });
+  var myPlacemarkTemp = new ymaps.GeoObject({
+    geometry: {
+        type: "Point",
+        coordinates: [44.679423, 37.627068] // координаты, где будет размещаться флажок на карте
+    }
+  });
+  myMapTemp.geoObjects.add(myPlacemarkTemp);
+  var layer = myMapTemp.layers.get(0).get(0);
+  waitForTilesLoad(layer).then(function() {
+    spinner.removeClass('is-active');
+  });
+}
+function waitForTilesLoad(layer) {
+  return new ymaps.vow.Promise(function (resolve, reject) {
+    var tc = getTileContainer(layer), readyAll = true;
+    tc.tiles.each(function (tile, number) {
+      if (!tile.isReady()) {
+        readyAll = false;
+      }
+    });
+    if (readyAll) {
+      resolve();
+    } else {
+      tc.events.once("ready", function() {
+        resolve();
+      });
+    }
+  });
+}
+function getTileContainer(layer) {
+  for (var k in layer) {
+    if (layer.hasOwnProperty(k)) {
+      if (
+        layer[k] instanceof ymaps.layer.tileContainer.CanvasContainer
+        || layer[k] instanceof ymaps.layer.tileContainer.DomContainer
+      ) {
+        return layer[k];
+      }
+    }
+  }
+  return null;
+}
+function loadScript(url, callback){
+  var script = document.createElement("script");
+
+  if (script.readyState){  // IE
+    script.onreadystatechange = function(){
+      if (script.readyState == "loaded" ||
+              script.readyState == "complete"){
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {  // Другие браузеры
+    script.onload = function(){
+      callback();
+    };
+  }
+
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+var ymap = function() {
+  $('.ymap-container').mouseenter(function(){
+      if (!check_if_load) {
+        check_if_load = true; 
+        spinner.addClass('is-active');
+        loadScript("https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;loadByRequire=1", function(){
+           ymaps.load(init);
+        });                
+      }
+    }
+  );  
+}
+$(function() {
+  ymap();
 
 });

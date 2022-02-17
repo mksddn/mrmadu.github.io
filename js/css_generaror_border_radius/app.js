d45cda980be50434f4be462f99c6979c2b2786fa
madu <mr.madu@ya.ru>
2022-02-17 21:37:19 +0300

    update cssborder

--- js/css_generaror_border_radius/app.js
+++ js/css_generaror_border_radius/app.js
@@ -1,6 +1,11 @@
 const inputRange = document.querySelectorAll('input.range'),
       inputText = document.querySelectorAll('input.text');
 
+const inputTL = document.getElementById('rangeTL'),
+      inputTR = document.getElementById('rangeTR'),
+      inputBL = document.getElementById('rangeBL'),
+      inputBR = document.getElementById('rangeBR');
+
 const square = document.getElementById('square');
 
 const resultTL = document.getElementById('resultTL'),
@@ -13,29 +18,42 @@ changeRadius(inputRange, 'input.text');
 changeRadius(inputText, 'input.range');
 
 
+// function changeRadius(el, input) {
+//     for(let [i, node] of el.entries()) {
+//         node.addEventListener('input', function() {
+//             const closest = this.closest('div').querySelector(input);
+//             let val = this.value;
+//             closest.value = val;
+//             if (i === 0) {
+//                 square.style.borderTopLeftRadius = `${val}px`;
+//                 getResult(resultTL, val);
+//             } else if (i === 1) {
+//                 square.style.borderTopRightRadius = `${val}px`;
+//                 getResult(resultTR, val);
+//             } else if (i === 2) {
+//                 square.style.borderBottomLeftRadius = `${val}px`;
+//                 getResult(resultBR, val);
+//             } else if (i === 3) {
+//                 square.style.borderBottomRightRadius = `${val}px`;
+//                 getResult(resultBL, val);
+//             }
+//         });
+//     }
+// }
+
+// function getResult(el, val) {
+//     el.innerHTML = val;
+// }
+
 function changeRadius(el, input) {
-    for(let [i, node] of el.entries()) {
+    for(let node of el) {
         node.addEventListener('input', function() {
             const closest = this.closest('div').querySelector(input);
             let val = this.value;
             closest.value = val;
-            if (i === 0) {
-                square.style.borderTopLeftRadius = `${val}px`;
-                getResult(resultTL, val);
-            } else if (i === 1) {
-                square.style.borderTopRightRadius = `${val}px`;
-                getResult(resultTR, val);
-            } else if (i === 2) {
-                square.style.borderBottomLeftRadius = `${val}px`;
-                getResult(resultBL, val);
-            } else if (i === 3) {
-                square.style.borderBottomRightRadius = `${val}px`;
-                getResult(resultBR, val);
-            }
+            let reStyle = inputTL.value + 'px ' + inputTR.value + 'px ' + inputBR.value + 'px ' + inputBL.value + 'px';
+            square.style.borderRadius = reStyle;
+            document.getElementById('result').innerHTML = reStyle;
         });
     }
-}
-
-function getResult(el, val) {
-    el.innerHTML = val;
 }

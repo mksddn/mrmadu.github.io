1e188efb78e5f416582a12717c47ce51c317d7bb
madu <mr.madu@ya.ru>
2022-03-09 16:04:30 +0300

    edit3

--- redform/app.js
+++ redform/app.js
@@ -34,13 +34,29 @@ function myFunction(x) {
     // nextArrow: '<button class="slick-next"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"  viewBox="0 0 477.175 477.175" xml:space="preserve">    <path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225 c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"/></svg></button>',
   });
 
-  $('.double-slider').slick({
+  $('.ds-first .double-slider').slick({
     // dots: true,
     infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
     speed: 300,
     slidesToShow: 1,
-    prevArrow: $('.product-types .slider-arrows .prev'),
-    nextArrow: $('.product-types .slider-arrows .next'),
+    prevArrow: $('.ds-first .slider-arrows .prev'),
+    nextArrow: $('.ds-first .slider-arrows .next'),
+    // adaptiveHeight: true,
+    // arrows: false,
+    // dotsClass: "my-dots",
+    // autoplay: true,
+  });
+  $('.ds-sec .double-slider').slick({
+    // dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 300,
+    slidesToShow: 1,
+    prevArrow: $('.ds-sec .slider-arrows .prev'),
+    nextArrow: $('.ds-sec .slider-arrows .next'),
     // adaptiveHeight: true,
     // arrows: false,
     // dotsClass: "my-dots",

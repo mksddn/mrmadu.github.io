03f22c43ce18c9f1ebd62f2707d2bc115aa7929b
madu <mr.madu@ya.ru>
2023-01-18 19:12:03 +0300

    add error page

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -7,28 +7,10 @@
 <script>
 export default {
   layout: 'post',
-  async asyncData({ app, store, params, route }) {
-    // if (
-    //   !store.state.currPost ||
-    //   route.params.slug !== store.state.currPost.slug
-    // ){
-
-    const { data } = await app.$axios.get(
-      `${process.env.VUE_APP_WP_API_URL}/wp/v2/posts`,
-      {
-        params: {
-          slug: params.slug,
-          _embed: true,
-        },
-      }
-    )
-    if (data[0] && data[0].type === 'post') {
-      store.commit('SET_CURR_POST', data[0])
-      const post = data[0]
-      return { post, type: 'post' }
-    } else {
+  async asyncData({ app, store, params, route, error }) {
+    try {
       const { data } = await app.$axios.get(
-        `${process.env.VUE_APP_WP_API_URL}/wp/v2/pages`,
+        `${process.env.VUE_APP_WP_API_URL}/wp/v2/posts`,
         {
           params: {
             slug: params.slug,
@@ -36,17 +18,33 @@ export default {
           },
         }
       )
-      store.commit('SET_CURR_POST', data[0])
-      const post = data[0]
-      return { post, type: 'page' }
+      if (data[0] && data[0].type === 'post') {
+        store.commit('SET_CURR_POST', data[0])
+        const post = data[0]
+        // console.log('post:' + post);
+        return { post, type: 'post' }
+      } else {
+        const { data } = await app.$axios.get(
+          `${process.env.VUE_APP_WP_API_URL}/wp/v2/pages`,
+          {
+            params: {
+              slug: params.slug,
+              _embed: true,
+            },
+          }
+        )
+        store.commit('SET_CURR_POST', data[0])
+        const post = data[0]
+        // console.log('page:' + post);
+        return { post, type: 'page' }
+      }
+    } catch (error) {
+      error({ statusCode: 404, message: 'Post not found' })
     }
-
-    // } else {
-    //   return { post: store.state.currPost }
-    // }
   },
   data: () => ({
     post: null,
+    type: 'error'
   }),
 }
 </script>

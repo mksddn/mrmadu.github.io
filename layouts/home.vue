71c434fd04044a6254c3820a538c54ed0153e57f
madu <mr.madu@ya.ru>
2023-02-02 19:33:45 +0300

    clean repo

--- layouts/home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <TheHeader />
-    <main class="main">
-      <Nuxt />
-    </main>
-    <TheFooter />
-  </div>
-</template>
--- pages/_slug/_slug.vue
+++
@@ -1,5 +0,0 @@
-<!-- ~/pages/_slug -->
-<script>
-import _slug from '~/pages/_slug'
-export default _slug
-</script>
